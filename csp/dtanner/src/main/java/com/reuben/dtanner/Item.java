75e50ecc3baa36663be64fa8ae12e3550006c54e
Reuben Tanner <reuben123@gmail.com>
2013-11-06 18:09:35 -0700

    Update Item.java
    in class changes, may not compile

--- csp/dtanner/src/main/java/com/reuben/dtanner/Item.java
+++ csp/dtanner/src/main/java/com/reuben/dtanner/Item.java
@@ -9,15 +9,26 @@ public class Item {
 
     private String name;
     private int size;
-    private char posneg;
-    private List<String> items;
+    private Boolean posneg;
+    private HashSet<String> items;
 
-    public Item(String name, int size, char posneg, List<String> items)
+    public Item(String name, int size, Boolean posneg)
     {
         this.name = name;
         this.size = size;
         this.posneg = posneg;
-        this.items = items;
+        items = new HashSet<String>();
+    }
+    
+    public void addConstraint(Item item)
+    {
+        items.add(item);
+    }
+    
+    public boolean canPackWith(Item i)
+    {
+        if (posneg == null) return true;
+        return items.contains(i) ? true : false; 
     }
 
     public String getName()
@@ -40,23 +51,29 @@ public class Item {
         this.size = size;
     }
 
-    public char getPosneg()
+    public Boolean getPosneg()
     {
         return posneg;
     }
 
-    public void setPosneg(char posneg)
+    public void setPosneg(Boolean posneg)
     {
         this.posneg = posneg;
     }
 
-    public List<String> getItems()
+    public HashSet<String> getItems()
     {
         return items;
     }
 
-    public void setItems(List<String> items)
+    public void setItems(HashSet<String> items)
     {
         this.items = items;
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        return name.hashCode();
     }
 }
